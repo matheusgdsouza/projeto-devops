89c3fb057f9c15bcb38f57f6b7ed798fbe856a24
matheusgdsouza <matheusgds3011@gmail.com>
2025-04-13 11:25:43 -0300

    Push git

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
-
+import random
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/teste1")
-async def root():
-    return {"message": "DEu certo."}
+@app.get("/funcao-teste")
+async def funcaoteste():
+    return {"teste": True, "num_aleatorio": random.randint(1, 100) }
